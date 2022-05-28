140bdf001e1be7934cf7d2acd53eeb4ac1a1e085
Wani Nz <missa_lastfera@yahoo.com>
2022-05-28 13:29:49 +0800

    Chek out time

--- src/app/pages/mukadepan/mukadepan.page.ts
+++ src/app/pages/mukadepan/mukadepan.page.ts
@@ -19,7 +19,6 @@ import { ChangeDetectorRef } from '@angular/core';
 })
 export class MukadepanPage implements OnInit {
 
-  showStatus: boolean = false;
   today = new Date();
   public myimage = 'assets/images/clock_in.png';
 
@@ -37,18 +36,17 @@ export class MukadepanPage implements OnInit {
   public displayUserData2: any[] = [];
   userInfo:any={};
   sid:any;
+  sname:any;
+  location:any;
 
   public checkData = {
     staff_id: '',
     action: 2,
   }
-  
 
-  leaveDetail:any={}
-  location:any;
 
   async settingsPopover(ev: any) {
-    const siteInfo = { id: 1, name: 'edupala' };
+    const siteInfo = { id: 1, name: 'hrapps dynaconsurv' };
     const popover = await this.popoverController.create({
       component: SettingComponent,
       event: ev,
@@ -67,6 +65,7 @@ export class MukadepanPage implements OnInit {
     /** Sync event from popover component */
 
   }
+
   constructor(private router: Router,
     private toastCtrl: ToastController, 
     private authService: AuthService,
@@ -85,6 +84,163 @@ export class MukadepanPage implements OnInit {
     this.nav.navigateRoot('home/mukadepan');
   }
 
+  async ionViewWillEnter()
+  {
+
+    let loader=await this.loading.create({
+      message:'Loading...',
+      spinner:'bubbles'
+    })
+
+    loader.present();
+
+    this.authService.getUserDataPromise().then((res: any) => {
+      this.displayUserData = res;
+      // this.checkData.staff_id = this.displayUserData.staff_id; 
+      this.sid = this.displayUserData.staff_id;
+      this.sname = this.displayUserData.staff_name;
+ 
+    this.authService.listName({staff_id: this.sid, action: 2}).subscribe((res: any) => {
+      console.log('goChecking',res);
+      let temp : any[] = [];
+      (res.results).forEach(function (value) {
+        console.log('value',value);
+
+        var dt = new Date(value.checkin);
+        //console.log("checkin time 1",dt)
+        const checkoutTime = dt.setMinutes( dt.getMinutes() + 540 );
+        let checkout = new Date(checkoutTime).toISOString();
+
+        temp.push({
+          staff_name: value.staff_name,
+          staff_id: value.staff_id,
+          checkin: new Date(value.checkin),
+          checkout: new Date(checkout),
+          location: value.location,
+        })
+      });
+      this.displayUserData2 = temp;
+      loader.dismiss();
+      console.log("user data 2",this.displayUserData2);} )
+
+    });
+
+  }
+
+  startTime() {
+    var intervalVar = setInterval(function () {
+    this.today = new Date().toISOString();
+  }.bind(this),500)}
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+    
+    // location.reload();     //// this serves the purpose by reloading the entire app but its not what i want
+
+    // this.navCtrl.navigateRoot('/tabs/tab2');  //// I tried this but not able to update 
+  }
+  
+  addEvent(){
+  	if(this.myimage=='assets/images/clock_in.png')
+  	{
+      this.showToast1();
+      this.checkInAction();
+      this.changeRef.detectChanges();
+  		this.myimage='assets/images/clock_out.png';
+      this.nav.navigateRoot('home/mukadepan');
+  	}else{
+      this.showToast2();
+      this.checkOutAction();
+      this.changeRef.detectChanges();
+		  this.myimage = 'assets/images/clock_in.png';
+  	}
+  	
+  }
+
+  checkInAction(){
+    this.postData.staff_id = this.displayUserData.staff_id;
+    this.postData.staff_name = this.displayUserData.staff_name;
+    this.postData.action = 0;
+    this.postData.location= this.location;
+    this.authService.checkIn(this.postData).subscribe((res: any) => {
+      this.authService.getUserData();
+      this.changeRef.detectChanges();
+      console.log('checkin',res)} )  
+  }
+
+  checkOutAction(){
+    this.postData.staff_id = this.displayUserData.staff_id;
+    this.postData.staff_name = this.displayUserData.staff_name;
+    this.postData.action = 1;
+    this.postData.location = this.location.data;
+    this.authService.checkOut(this.postData).subscribe((res: any) => {
+      this.authService.getUserData();
+      this.changeRef.detectChanges();
+      console.log('checkout',res)} )
+  }
+
+  async showToast1(){
+    await this.toastCtrl.create({
+      message: "You've Check-In!",
+      duration: 2000,
+      position: 'middle',
+      buttons: [{
+        text: 'OK'
+      }]
+    }).then( res => res.present());
+    // document.getElementById("status").innerHTML = "You've Checked-In!";
+  }
+
+  async showToast2(){
+    await this.toastCtrl.create({
+      message: "You've Check-Out!",
+      duration: 2000,
+      position: 'middle',
+      buttons: [{
+        text: 'OK'
+      }]
+    }).then( res => res.present());
+    // document.getElementById("status").innerHTML = "You've Checked-Out!";
+  }
+
+  async showToast3(){
+    await this.toastCtrl.create({
+      message: "You're on Leave!",
+      duration: 2000,
+      position: 'middle',
+      buttons: [{
+        text: 'OK'
+      }]
+    }).then( res => res.present());
+  }
+
+  goToList(){
+    this.postData.staff_id = this.displayUserData.staff_id;
+    this.postData.action = 2;
+    this.router.navigate(['listattendees']);
+    this.authService.listName(this.postData).subscribe((res: any) => {
+      console.log('golist',res)} )
+  }
+
+  refresh() {
+    this.changeRef.detectChanges();
+  }
+
+}
+
+
+
+
+
+
+
+/*** 
+  showStatus: boolean = false;
   async ionViewWillEnter()
   {
 
@@ -118,107 +274,5 @@ export class MukadepanPage implements OnInit {
     });
 
   }
-
-  startTime() {
-    var intervalVar = setInterval(function () {
-    this.today = new Date().toISOString();
-  }.bind(this),500)}
-
-  doRefresh(event) {
-    console.log('Begin async operation');
-
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.target.complete();
-    }, 2000);
-    
-    // location.reload();     //// this serves the purpose by reloading the entire app but its not what i want
-
-    // this.navCtrl.navigateRoot('/tabs/tab2');  //// I tried this but not able to update 
-  }
-
-  addEvent(){
-  	if(this.myimage=='assets/images/clock_in.png')
-  	{
-      this.showToast1();
-      this.checkInAction();
-      this.changeRef.detectChanges();
-  		this.myimage='assets/images/clock_out.png';
-      this.nav.navigateRoot('home/mukadepan');
-  	}else{
-      this.showToast2();
-      this.checkOutAction();
-      this.changeRef.detectChanges();
-		  this.myimage = 'assets/images/clock_in.png';
-  	}
-  	
-  }
-
- checkInAction(){
-    this.postData.staff_id = this.displayUserData.staff_id;
-    this.postData.action = 0;
-    this.postData.location= this.location;
-    this.authService.checkIn(this.postData).subscribe((res: any) => {
-      this.authService.getUserData();
-      this.changeRef.detectChanges();
-      console.log('checkin',res)} )  
 }
-
-  checkOutAction(){
-     this.postData.staff_id = this.displayUserData.staff_id;
-     this.postData.action = 1;
-     this.postData.location = this.location.data;
-    this.authService.checkOut(this.postData).subscribe((res: any) => {
-      this.authService.getUserData();
-      this.changeRef.detectChanges();
-      console.log('checkout',res)} )
-  }
-
-  async showToast1(){
-    await this.toastCtrl.create({
-      message: "You've Check-In!",
-      duration: 2000,
-      position: 'middle',
-      buttons: [{
-        text: 'OK'
-      }]
-    }).then( res => res.present());
-    // document.getElementById("status").innerHTML = "You've Checked-In!";
-  }
-
-  async showToast2(){
-    await this.toastCtrl.create({
-      message: "You've Check-Out!",
-      duration: 2000,
-      position: 'middle',
-      buttons: [{
-        text: 'OK'
-      }]
-    }).then( res => res.present());
-    // document.getElementById("status").innerHTML = "You've Checked-Out!";
-  }
-
- async showToast3(){
-    await this.toastCtrl.create({
-      message: "You're on Leave!",
-      duration: 2000,
-      position: 'middle',
-      buttons: [{
-        text: 'OK'
-      }]
-    }).then( res => res.present());
-  }
-
-  goToList(){
-    this.postData.staff_id = this.displayUserData.staff_id;
-    this.postData.action = 2;
-    this.router.navigate(['listattendees']);
-    this.authService.listName(this.postData).subscribe((res: any) => {
-      console.log('golist',res)} )
-  }
-
-  refresh() {
-    this.changeRef.detectChanges();
-  }
-  
-}
+  ***/
