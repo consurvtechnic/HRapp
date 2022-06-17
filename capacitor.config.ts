5f1cb09bf627999e6edffcae8d04eb2bab63c16b
Wani Nz <missa_lastfera@yahoo.com>
2022-06-17 12:23:29 +0800

    capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,31 +1,29 @@
-/// <reference types="@capacitor/local-notifications" />
-/// <reference types="@capacitor/push-notifications" />
-/// <reference types="@capacitor/splash-screen" />
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "io.consurv.HRApp",
-  appName: "HRApp",
+  appId: 'io.consurv.HRApp',
+  appName: 'HRApp',
   bundledWebRuntime: false,
-  //npmClient: npm,
+  //npmClient: "npm",
   //allowMixedContent: true,
-  webDir: 'build',
-  //webDir: "www",
-  plugins: {
-    SplashScreen: {
-      launchAutoHide: false,
-      launchShowDuration: 0
+  webDir: 'www',
+  "plugins": {
+    "SplashScreen": {
+      "launchAutoHide": false,
+      "launchShowDuration": 0
     },
-    LocalNotifications: {
-      smallIcon: 'ic_stat_icon_config_sample',
-      iconColor: '#CE0B7C',
+    "LocalNotifications": {
+      "smallIcon": 'ic_stat_icon_config_sample',
+      "iconColor": '#CE0B7C',
     },
-    PushNotifications: {
-      presentationOptions: ['alert', 'sound'],
+    "PushNotifications": {
+      "presentationOptions": ['alert', 'sound'],
     },
   }
   //cordova: {},
-}
+};
 
 export default config;
+
+
+  
