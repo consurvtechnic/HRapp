97a0101d33f2fa7a007f8ed11bef84713af3a734
Wani Nz <missa_lastfera@yahoo.com>
2022-09-08 12:22:09 +0800

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,28 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [ 
-    FormsModule, BrowserModule, IonicModule.forRoot({ mode: 'md',}), AppRoutingModule, HttpClientModule],
+    FormsModule, 
+    BrowserModule, 
+    IonicModule.forRoot({ mode: 'md',}), 
+    AppRoutingModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule],
+    
   providers: [HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy,}],
   bootstrap: [AppComponent]
 })
